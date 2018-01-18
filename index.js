352a168c710be21716ada68b54da00619b4c8682
fengfff <865553742@qq.com>
2018-01-18 14:19:11 +0800

    debug

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 
-app.use("/api", require("./routes/qqmusic"));
+// app.use("/api", require("./routes/qqmusic"));
 var app = express();
 
 express()
   .use(express.static(path.join(__dirname, "public")))
-  .set("views", path.join(__dirname, "views"))
   .use("/api", require("./routes/qqmusic"))
+  .set("views", path.join(__dirname, "views"))
   .set("view engine", "ejs")
   .get("/", (req, res) => res.render("pages/index"))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
