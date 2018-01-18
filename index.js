f9065564bdb735a05ef94315694d2a9c6ea8a611
fengfff <865553742@qq.com>
2018-01-18 14:26:26 +0800

    em

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 5000
 
 
 // app.use("/api", require("./routes/qqmusic"));
-var app = express();
+// var app = express();
 
 express()
   .use(express.static(path.join(__dirname, "public")))
